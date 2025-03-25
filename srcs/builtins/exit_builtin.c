818fd8db27273853e9d6a92fef18b7ddb03749ad
Mark Byrne <markbyrne477@gmail.com>
2025-03-25 15:01:53 +0000

    fixed still reachable memory upon exit

--- srcs/builtins/exit_builtin.c
+++ srcs/builtins/exit_builtin.c
@@ -63,36 +63,20 @@ static long long	convert_to_number(const char *str, int *overflow)
 	return (result * sign);
 }
 
-static void	handle_invalid_arg(t_mini *mini, char *arg)
+static int	handle_exit_error(t_mini *mini, int error_type, char *arg)
 {
-	ft_putstr_fd("minishell: exit: ", STDERR_FILENO);
-	ft_putstr_fd(arg, STDERR_FILENO);
-	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-	mini->exit_status = 2;
-	clean_exit(mini);
-}
-
-static int	handle_sign_and_number(t_mini *mini, char **args, int arg_count)
-{
-	char		*num_str;
-	long long	exit_code;
-	int			overflow;
-
-	num_str = ft_strjoin(args[1], args[2]);
-	if (!num_str)
-		return (ERROR);
-	exit_code = convert_to_number(num_str, &overflow);
-	free(num_str);
-	if (overflow)
-		handle_invalid_arg(mini, args[2]);
-	if (arg_count > 3)
+	if (error_type == 1)
 	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
+		ft_putstr_fd("minishell: exit: too many arguments\n", 2);
 		mini->exit_status = 1;
-		return (1);
 	}
-	mini->exit_status = (unsigned char)exit_code;
-	clean_exit(mini);
+	else
+	{
+		ft_putstr_fd("minishell: exit: numeric argument required\n", 2);
+		mini->exit_status = 2;
+		mini->should_exit = 1;
+	}
+	(void)arg;
 	return (0);
 }
 
@@ -106,21 +90,18 @@ int	exit_builtin(t_mini *mini, char **args)
 	while (args[arg_count])
 		arg_count++;
 	if (arg_count == 1)
-		clean_exit(mini);
-	if (arg_count >= 3 && (ft_strcmp(args[1], "+") == 0
-			|| ft_strcmp(args[1], "-") == 0) && is_numeric(args[2]))
-		return (handle_sign_and_number(mini, args, arg_count));
+	{
+		mini->should_exit = 1;
+		return (0);
+	}
 	if (!is_numeric(args[1]))
-		handle_invalid_arg(mini, args[1]);
+		return (handle_exit_error(mini, 2, args[1]));
+	if (arg_count > 2)
+		return (handle_exit_error(mini, 1, NULL));
 	exit_code = convert_to_number(args[1], &overflow);
 	if (overflow)
-		handle_invalid_arg(mini, args[1]);
-	if (arg_count > 2)
-	{
-		ft_putstr_fd("minishell: exit: too many arguments\n", STDERR_FILENO);
-		mini->exit_status = 1;
-		return (1);
-	}
+		return (handle_exit_error(mini, 2, args[1]));
 	mini->exit_status = (unsigned char)exit_code;
-	return (clean_exit(mini), 0);
+	mini->should_exit = 1;
+	return (0);
 }
